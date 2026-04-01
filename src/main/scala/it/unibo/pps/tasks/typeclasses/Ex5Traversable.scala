d6d6a9756f1cab2a7c5a98a91151fdaf9665321e
Margherita Zanchini <margherita.zanchini@studio.unibo.it>
2026-04-01 18:48:40 +0200

    task 5 completed

--- src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
+++ src/main/scala/it/unibo/pps/tasks/typeclasses/Ex5Traversable.scala
@@ -1,6 +1,7 @@
 package it.unibo.pps.tasks.typeclasses
 
-import it.unibo.pps.u03.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.extensionmethods.Sequences.Sequence, Sequence.*
+import it.unibo.pps.u03.extensionmethods.Optionals.Optional, Optional.*
 
 /*  Exercise 5: 
  *  - Generalise by ad-hoc polymorphism logAll, such that:
@@ -19,8 +20,29 @@ object Ex5Traversable:
 
   def log[A](a: A): Unit = println("The next element is: "+a)
 
-  def logAll[A](seq: Sequence[A]): Unit = seq match
-    case Cons(h, t) => log(h); logAll(t)
-    case _ => ()
+  def logAll[T[_]: Traversable, A](n: T[A]): Unit =
+    val traversable = summon[Traversable[T]]
+    traversable.consumer(n)(log)
 
-  
+  trait Traversable[T[_]]:
+    def consumer[A](t: T[A])(f: A => Unit): Unit
+
+  given Traversable[Optional] with
+    def consumer[A](t: Optional[A])(f: A => Unit): Unit = t match {
+      case Just(a) => f(a)
+      case _ => ()
+    }
+
+  given Traversable[Sequence] with
+    def consumer[A](t: Sequence[A])(f: A => Unit): Unit = t match {
+      case Cons(head, tail) => f(head); consumer(tail)(f)
+      case _ => ()
+    }
+
+    @main def tryTraversable =
+      val si = Cons(10, Cons(20, Cons(30, Nil())))
+      logAll(si)
+      
+      logAll(Just(15))
+      logAll(None())
+
